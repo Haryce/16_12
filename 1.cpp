4c437c85d8a8f982c24f0e0bcbea198423c886d9
Haryce <148765923+Haryce@users.noreply.github.com>
2024-12-16 16:49:56 +0900

    Add files via upload

---
+++ 1.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+using namespace std;
+void Swap(int* a, int size) {
+    for (int i = 0; i < size - 1; i += 2) {
+        int temp = *(a + i);
+        *(a + i) = *(a + i + 1);
+        *(a + i + 1) = temp;
+    }
+}
+int main() {
+    setlocale(LC_ALL, "Russian");
+    const int size = 10; 
+    int a[size] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 }; 
+
+    cout << "Исходный массив: ";
+    for (int i = 0; i < size; ++i) {
+        cout << a[i] << " ";
+    }
+    cout << endl;
+    Swap(a, size);
+    cout << "Массив после замены: ";
+    for (int i = 0; i < size; ++i) {
+        cout << a[i] << " ";
+    }
+    cout << endl;
+    return 0;
+}
